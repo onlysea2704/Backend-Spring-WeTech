fcf95c47263feb616cd2be24550f1d8063d3489d
onlysea2704 <phamduyhai2704@gmail.com>
2025-09-25 00:54:11 +0700

    m

--- src/main/java/com/wetech/backend_spring_wetech/controller/AuthController.java
+++ src/main/java/com/wetech/backend_spring_wetech/controller/AuthController.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -45,5 +46,13 @@ public class AuthController {
     @GetMapping("/protected")
     public ResponseEntity<?> protectedEndpoint() {
         return ResponseEntity.ok("This is a protected endpoint");
+    }
+
+    @GetMapping("/get-info")
+    public ResponseEntity<?> getUserInfo() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String username = authentication.getName();
+        User user = (User) userService.loadUserByUsername(username);
+        return ResponseEntity.ok(user);
     }
 }
---
+++ src/main/java/com/wetech/backend_spring_wetech/dto/UserDto.java
@@ -0,0 +1,7 @@
+package com.wetech.backend_spring_wetech.dto;
+
+import lombok.Data;
+
+@Data
+public class UserDto {
+}
