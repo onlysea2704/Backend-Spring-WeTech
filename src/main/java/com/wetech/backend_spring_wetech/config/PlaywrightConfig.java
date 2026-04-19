11463c33d4ad76bc18a8a9fdbc91bd63d0151ee3
envidi3107 <duyn38520@gmail.com>
2026-04-19 11:05:01 +0700

    feat: configure Playwright and Chromium browser beans for Spring application context

--- src/main/java/com/wetech/backend_spring_wetech/config/PlaywrightConfig.java
+++ src/main/java/com/wetech/backend_spring_wetech/config/PlaywrightConfig.java
@@ -4,9 +4,6 @@ import com.microsoft.playwright.Browser;
 import com.microsoft.playwright.Playwright;
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,12 +12,7 @@ import org.springframework.context.annotation.Configuration;
 public class PlaywrightConfig {
     @Bean
     public Playwright playwright() {
-        log.info("Initializing Playwright instance");
-        Map<String, String> env = new HashMap<>();
-        // Skip the default behavior which downloads all browsers (Chromium, Firefox,
-        // WebKit)
-        env.put("PLAYWRIGHT_SKIP_BROWSER_DOWNLOAD", "1");
-        return Playwright.create(new Playwright.CreateOptions().setEnv(env));
+        return Playwright.create();
     }
 
     @Bean
